fd437cffd39f3b68555dae8290b140365010f439
tobioka <tobioka@ip-10-241-3-74.ap-northeast-1.compute.internal>
2023-11-12 19:06:46 +0900

    custom project

--- script.js
+++ script.js
@@ -106,3 +106,91 @@ Array.from(aboutMeTextContent).forEach(char => {
     })
 })
 // end of about me text
+
+//projects
+const container = document.querySelector(".container")
+const projects = document.querySelectorAll(".project")
+const projectHideBtn = document.querySelector(".project-hide-btn")
+
+projects.forEach((project,i) => {
+    project.addEventListener("mouseenter",()=> {
+        project.firstElementChild.style.top = `-${project.firstElementChild.offsetHeight - project.offsetHeight +20}px`;
+    });
+
+    project.addEventListener("mouseleave",() => {
+        project.firstElementChild.style.top = "2rem";
+    });
+
+    project.addEventListener("click",()=> {
+        const bigImgWrapper = document.createElement('div');
+        bigImgWrapper.className = "project-img-wrapper";
+        container.appendChild(bigImgWrapper);
+
+        const bigImg = document.createElement('img');
+        bigImg.className ="project-img";
+        const imgPath = project.firstElementChild.getAttribute("src").split(".")[0];
+        bigImg.setAttribute("src",`${imgPath}-big.jpg`)
+        bigImgWrapper.appendChild(bigImg)
+        document.body.style.overflow = "hidden"
+
+        projectHideBtn.classList.add('change')
+
+        projectHideBtn.onclick = () => {
+            projectHideBtn.classList.remove("change");
+            bigImgWrapper.remove()
+            document.body.style.overflowY = "scroll"
+        }
+    })
+
+    i >=6 &&(project.style.cssText = "display:none; opacity:0")
+    
+});
+
+//projects Button
+const section3 = document.querySelector(".section-3")
+const projectsBtn = document.querySelector('.projects-btn')
+const projectsBtnText = document.querySelector('.projects-btn span')
+let showHideBool = true
+
+const showProjects = (project,i) => {
+    setTimeout(()=> {
+        project.style.display ="flex";
+        section3.scrollIntoView({block:"end"})
+    },600);
+
+    setTimeout(() => {
+        project.style.display ="flex";
+        project.style.opacity ="1";
+    },i*200)
+};
+
+const hideProjects = (project,i) => {
+    setTimeout(()=> {
+        project.style.display ="none"
+    },1200)
+    section3.scrollIntoView({block:"end"})
+
+    setTimeout(()=> {
+        project.style.opacity ="0"
+    },i * 100)
+    project.style.display ="node"
+    project.style.opacity = "0"
+    
+}
+
+projectsBtn.addEventListener('click', (e) => {
+e.preventDefault()
+
+projectsBtn.firstElementChild.nextElementSibling.classList.toggle("change")
+
+showHideBool ? (projectsBtnText.textContent = "Show Less"):(projectsBtnText.textContent = "Show More")
+
+projects.forEach((project,i) => {
+    i >=6 &&(showHideBool ? showProjects(project,i): hideProjects(project,i))
+
+});
+showHideBool = !showHideBool;
+})
+//end of projects Button
+
+//end of projects

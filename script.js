4bfeb3b6a805c213f7ac34cb0e81bd7dd83cdd82
tobioka <tobioka@ip-10-241-3-74.ap-northeast-1.compute.internal>
2023-11-14 11:43:07 +0900

    form

--- script.js
+++ script.js
@@ -211,5 +211,62 @@ document.querySelectorAll(".service-btn").forEach((service) => {
 
 //end of section4
 
+//section5
+// Form
+const formHeading = document.querySelector(".form-heading");
+const formInputs = document.querySelectorAll(".contact-form-input");
+
+formInputs.forEach((input) => {
+  input.addEventListener("focus", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = `Your ${input.placeholder}`;
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+
+  input.addEventListener("blur", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = "Let's Talk";
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+});
+// End of Form
+
+//slideshow
+const slideshow = document.querySelector('.slideshow')
+
+setInterval(() => {
+    const firstIcon = slideshow.firstElementChild
+
+    firstIcon.classList.add('faded-out');
+
+    const thirdIcon = slideshow.children[3];
+    thirdIcon.classList.add("light");
+
+    thirdIcon.previousElementSibling.classList.remove("light")
+
+
+    setTimeout(() => {
+        slideshow.removeChild(firstIcon);
+
+        slideshow.appendChild(firstIcon);
+
+        setTimeout(()=> {
+            firstIcon.classList.remove("faded-out")
+        },500)
+
+        
+
+    },500)
+
+
+},3000)
+//end of slideshow
+
+//end ofsection5
+
 
 //end of projects
